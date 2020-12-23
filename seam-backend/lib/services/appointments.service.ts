4d611ca9274e64b1d3c02e49ab10d1587411d6a9
Samuel Poulin <samuel-poulin@outlook.com>
2020-12-23 13:05:50 -0500

    Added trigger to add providers on user creation and added post for appointments api

--- seam-backend/lib/services/appointments.service.ts
+++ seam-backend/lib/services/appointments.service.ts
@@ -2,12 +2,46 @@ import { inject, injectable } from 'inversify';
 import { Appointment } from '../models/appointment';
 import { DatabaseService } from './database.service';
 import Types from '../inversify/types';
+import { resolve } from 'path';
 
 @injectable()
 export class AppointmentsService {
   constructor(
     @inject(Types.DatabaseService) private databaseService: DatabaseService
   ) { }
+
+  async insertAppointment(
+    providerid: number,
+    title: string,
+    description: string,
+    location: string,
+    startTime: Date,
+    endTime: Date,
+    customerid: number
+  ): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      this.databaseService.pool
+        .query(
+          'CALL insertAppointment(?,?,?,?,?,?,?)',
+          [
+            providerid,
+            title,
+            description,
+            location,
+            startTime,
+            endTime,
+            customerid
+          ],
+          (err, rows) => {
+            if (err || !rows) {
+              reject(err);
+            } else {
+              resolve(rows[0][0].id);
+            }
+          }
+        );
+    });
+  }
 
   async getAppointmentsByUserId(userid: number): Promise<Appointment[]> {
     return new Promise<Appointment[]>((resolve, reject) => {
